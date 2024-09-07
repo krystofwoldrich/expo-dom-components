692cc12889fae4a6b17e571cfcb9c2242a401456
Krystof Woldrich <krystofwoldrich@gmail.com>
2024-09-07 20:38:06 +0200

    Add basic example server with build tools

--- lib/serverSentry.ts
+++ lib/serverSentry.ts
@@ -1,1 +1,12 @@
-// TODO:
+import * as Sentry from '@sentry/node';
+
+export function initSentry() {
+  // Ensure to call this before importing any other modules!
+  Sentry.init({
+    dsn: 'https://1df17bd4e543fdb31351dee1768bb679@o447951.ingest.us.sentry.io/5428561',
+
+    // Add Tracing by setting tracesSampleRate
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
